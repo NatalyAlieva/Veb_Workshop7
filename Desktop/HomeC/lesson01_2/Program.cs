f6ed9f328061181d790488664ad49a0cfad65b38
Nataly <luckina.natalia@yandex.ru>
2022-09-21 22:51:39 +0300

    Задача 4 семинара 2

---
+++ Desktop/HomeC/lesson01_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int i = 2;
+Console.Write("Введите N ");
+int N = Int32.Parse(Console.ReadLine());
+while (i < N)
+            {  
+                Console.WriteLine(Convert.ToString(i));
+                i = i+2;
+            }
+
+
