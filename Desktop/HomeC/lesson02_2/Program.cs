d58110766d7548cd21016a886c6502ba79a60121
Nataly <luckina.natalia@yandex.ru>
2022-09-25 23:26:15 +0300

    Семинар 2 Задача 2

---
+++ Desktop/HomeC/lesson02_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Clear();
+Console.Write("Введите число ");
+int a = Int32.Parse(Console.ReadLine());
+string str = a.ToString();
+int[] aint = new int[str.Length];
+
+if (str.Length > 2)
+{
+    aint[2] = int.Parse(str[2] + "");
+    Console.WriteLine(aint[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
