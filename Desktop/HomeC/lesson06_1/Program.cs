7747eb9e0f98a0d505f9272982971e00a18aedd8
Nataly <luckina.natalia@yandex.ru>
2022-10-10 18:48:37 +0300

    Семинар 6 Задача 1

---
+++ Desktop/HomeC/lesson06_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+string s=" ";
+int count=0;
+Console.WriteLine("Введите числа через запятую");
+s=Console.ReadLine();
+string[] pair=s.Split(',');
+int[] mas=new int[pair.Length]; 
+for (int i=0;i<mas.Length;i++)
+{ 
+    mas[i] = Convert.ToInt32(pair[i]);
+    if (mas[i]>0)   count++;
+}
+Console.WriteLine($"Количество чисел больше нуля {count}");
+
+
