fe1670ce2c27ea0d71644886574f4c8380112f72
Nataly <luckina.natalia@yandex.ru>
2022-10-20 13:55:16 +0300

    Create new array/result

--- Desktop/HomeC/lesson09/Program.cs
+++ Desktop/HomeC/lesson09/Program.cs
@@ -32,4 +32,49 @@ void PrintArray(char[,] mas)
     }
 }
 
+char[] ArraySymbol(char[,] array2D)
+{//определить количество символов в array2D для размерности нового массива
+    int count = 0;
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            if (array2D[i, j] >= '0' && array2D[i, j] <= '9')
+                Console.WriteLine($"Элемент [{i},{j}]={array2D[i, j]} является цифрой");
+            else
+                count++;
+        }
+    }
+  //конец определить количество символов в array2D для размерности нового массива
+    Console.WriteLine();
+    Console.WriteLine("Результирующий массив");
+    char[] array1D = new char[count];
+    int k = 0;
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            if (array2D[i, j]! >= '0' && array2D[i, j]! <= '9')
+            { }
+            else
+            {
+                array1D[k] = array2D[i, j];
+                Console.Write($"{array1D[k]}({k}) ");
+                k++;
+            }
+        }
+        Console.WriteLine();
+    }
+    return array1D;
+}
+
+
+char[,] matrix = ArrayChar();
+Console.WriteLine("Входной массив ");
+PrintArray(matrix);
+Console.WriteLine();
+char[] massiv = ArraySymbol(matrix);
+
+
+
 
