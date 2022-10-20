43e173db8116aa08e3e35d8a561652a56b6dbcbf
Nataly <luckina.natalia@yandex.ru>
2022-10-20 13:51:26 +0300

    Create array

--- Desktop/HomeC/lesson09/Program.cs
+++ Desktop/HomeC/lesson09/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из сформированного массива строк 
+//содержащих символы и числа создать массив из строк, в который 
+//попадут только символы, не являющиеcя цифрами int.
+
+char[,] ArrayChar()
+{
+int rows = new Random().Next(5, 10);
+int col = new Random().Next(5, 10);
+char[,] str = new char[rows, col];
+var r = new Random();
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < col; j++)
+    {    do
+
+                str[i,j] = (char)r.Next(127);
+            while (str[i,j] < '!');
+    }
+}
+    return str;
+}    
+
